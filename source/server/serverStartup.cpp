503e9df3ec3e477089aa4a5003a2b7a0f2213bd8
Ivo Hanak <hanak@ipp.cas.cz>
2024-05-15 09:49:36 +0200

    Enabled directory creation in Win32 systems

--- source/server/serverStartup.cpp
+++ source/server/serverStartup.cpp
@@ -1,3 +1,9 @@
+#ifdef _WIN32
+#include <windows.h>
+#include <Shlobj.h>
+#include <algorithm>
+#endif
+
 #include "serverStartup.h"
 
 #include <cstdlib>
@@ -22,11 +28,30 @@ int startup()
     udaSetLogLevel((LOG_LEVEL)environment->loglevel);
 
     if (environment->loglevel <= UDA_LOG_ACCESS) {
+        #ifdef _WIN32
+        std::string logdir(environment->logdir);
+        if (!logdir.empty() && logdir != "./") {
+            std::replace(logdir.begin(), logdir.end(), '/', '\\');
+            if (logdir.back() == '\\') {
+                logdir.resize(logdir.length() - 1);
+            }
+            SHCreateDirectoryExA(NULL, logdir.c_str(), NULL);
+            const DWORD lastError = GetLastError();
+            const DWORD logdirAttr = GetFileAttributesA(logdir.c_str());
+            if (logdirAttr == INVALID_FILE_ATTRIBUTES || !(logdirAttr & FILE_ATTRIBUTE_DIRECTORY)) {
+                // No logging facilities are set. Thus, no log will appear anywhere.
+                // Therefore, dump information to STDERR.
+                fprintf(stderr, "Creating log directory %s failed (last error %u).\n", environment->logdir, lastError);
+                UDA_THROW_ERROR(999, "creating log directory failed");
+            }
+        }
+        #else
         char cmd[STRING_LENGTH];
         snprintf(cmd, STRING_LENGTH, "mkdir -p %s 2>/dev/null", environment->logdir);
         if (system(cmd) != 0) {
             UDA_THROW_ERROR(999, "mkdir command failed");
         }
+        #endif
 
         errno = 0;
         std::string log_file = std::string{ environment->logdir } + "Access.log";
